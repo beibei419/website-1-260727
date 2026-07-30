386e1f0df8de612111646579df3843ee76bf9678
beibei419 <chenchengmei@163.com>
2026-07-30 17:55:16 -0400

    oumhgohd

---
+++ CLUB.js
@@ -0,0 +1,21 @@
+const username = document.querySelector('.username'); 
+const post = document.querySelector(",photo"); 
+const likes = document.querySelector(".likes"); 
+const comments = document.querySelector(".comments")
+
+
+posts: [
+
+{username:name1, 
+post: URL("https://static.vecteezy.com/system/resources/previews/011/288/267/original/golden-number-1-png.png"), 
+likes: "❤️ 15", 
+comments: ['1', '2']
+}, 
+
+
+
+]
+
+
+
+postindex: Math.floor(Math.random() * length(posts)) - 1; 
